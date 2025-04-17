c62de67030d59284729dd2bb5097d1853dc6e8b1
ImkePapsPXL <Imke.Paps@student.pxl.be>
2025-04-17 14:00:26 +0200

    aanpassing query

--- WPFTeam08/ClassLibrary1/Data/UserData.cs
+++ WPFTeam08/ClassLibrary1/Data/UserData.cs
@@ -446,7 +446,7 @@ namespace ClassLibTeam08.Data
             {
                 //SQL Command
                 StringBuilder insertQuery = new StringBuilder();
-                insertQuery.Append($"SELECT u.firstName, u.lastName, u.phone, u.email, u.rol, MAX(l.loginTime) AS lastLoginTime FROM users u JOIN logins l ON u.userID = l.userID WHERE u.rol = 'admin'GROUP BY  u.userID, u.firstName, u.lastName, u.phone, u.email, u.rol ORDER BY lastLoginTime DESC;");
+                insertQuery.Append($"SELECT u.firstName, u.lastName, u.adres, u.phone, u.email, u.rol, MAX(l.loginTime) AS lastLoginTime FROM users u JOIN logins l ON u.userID = l.userID GROUP BY  u.userID, u.firstName, u.lastName, u.adres,u.phone, u.email, u.rol ORDER BY lastLoginTime DESC;");
                 using (SqlCommand insertCommand = new SqlCommand(insertQuery.ToString()))
                 {
                     result = Select(insertCommand);
