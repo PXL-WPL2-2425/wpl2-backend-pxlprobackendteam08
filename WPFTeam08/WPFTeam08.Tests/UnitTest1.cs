bdec440f8df194392ed31f893c84b9cd2e539b2f
ThibaultLesecquePXL <thibault.lesecque@student.pxl.be>
2025-04-25 11:56:06 +0200

    Aanmaken NUNit testproject

---
+++ WPFTeam08/WPFTeam08.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class UnitTest1
+{
+    [Test]
+    public void TestMethod1()
+    {
+        int test = 3;
+        int result = test;
+
+        Assert.That(result, Is.EqualTo(3));
+    }
+}
